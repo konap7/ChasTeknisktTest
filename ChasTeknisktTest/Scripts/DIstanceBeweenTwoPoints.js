d34be23ef857fdb0a58abb18521e8b68ca85df50
Simon Llanso <cogliostro_g@hotmail.com>
2017-08-29 16:06:27 +0200

    Fungerande JavaScript

--- ChasTeknisktTest/Scripts/DIstanceBeweenTwoPoints.js
+++ ChasTeknisktTest/Scripts/DIstanceBeweenTwoPoints.js
@@ -1,37 +1,10 @@
 ﻿//Deklaration av variabler
 var start, end;
-//var directionsDisplay;
-//var directionsService = new google.maps.DirectionsService();
 
-//Ritar ut kartan centrerad över Stockholm
-//var map = new google.maps.Map(document.getElementById('Map'), {
-//    center: { lat: 59.3333333, lng: 18.05 },
-//    zoom: 13,
-//    mapTypeId: 'roadmap'
-//});
-
-//Applicerar "type ahead" på de två inmatningsfälten så att det blir enklare att söka platser
-//google.maps.event.addDomListener(window, 'load', function () {
-//    new google.maps.places.SearchBox(document.getElementById('Start'));
-//    new google.maps.places.SearchBox(document.getElementById('End'));
-//});
 
 //Hämtar in det användaren matade in för att skicka till API:et 
 start = document.getElementById("Start").value;
 end = document.getElementById("End").value;
-
-//var request = {
-//    origin: start,
-//    destination: end,
-//    travelMode: google.maps.TravelMode.DRIVING
-//};
-
-////Ritar ut användarens route
-//directionsService.route(request, function (response, status) {
-//    if (status == google.maps.DirectionsStatus.OK) {
-//        directionsDisplay.setDirections(response);
-//    }
-//});
 
 //Anropar API:ets DistanceMatrixService för att kunna räkna ut avstånd mellan punkterna samt den tid det kommer ta att färdas sträckan
 var service = new google.maps.DistanceMatrixService();
@@ -43,14 +16,17 @@ service.getDistanceMatrix({
 }, callback);
 
 function callback(response, status) {
-    if (status == google.maps.DistanceMatrixStatus.OK && 
+    if (status == google.maps.DistanceMatrixStatus.OK &&
                   response.rows[0].elements[0].status != "ZERO_RESULTS") {
         var distance = response.rows[0].elements[0].distance.text;
         var duration = response.rows[0].elements[0].duration.value;
         var dvDistance = document.getElementById("Distance");
-        duration = parseFloat(duration / 60).toFixed(2);
+        //duration = parseFloat(duration / 3600).toFixed(2);
+        var hours = Math.floor(duration / 3600);
+        duration %= 3600;
+        var minutes = Math.floor(duration / 60);
         dvDistance.innerHTML = "";
         dvDistance.innerHTML += "Distance: " + distance + "<br />";
-        dvDistance.innerHTML += "Time: " + duration + " min";
+        dvDistance.innerHTML += "Time: " + hours + "h" + minutes + "min";
     }
 }
